5a252ee4a2a026741d23ce81b14c5ea97be8cbdc
Mohit Bhatt <bhattmohit869@gmail.com>
2024-03-08 20:22:59 +0530

    changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<App/>
+{/* added by me */}
+<ToastContainer position="top-right" />  
+</React.StrictMode>
+)
